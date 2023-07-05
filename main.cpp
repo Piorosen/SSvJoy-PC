730d049a85699c6e43056dee22278aa4b5edfbfd
Piorosen <chacha@udon.party>
2023-07-06 01:06:21 +0900

    데이터 아규먼트 추가함. 기본값과 cxxopts 조건 추가

--- main.cpp
+++ main.cpp
@@ -13,12 +13,18 @@ int main(int argc, char** argv){
     cxxopts::Options options("SSvJoy", "HTTP 통신으로 당신의 조이스틱을 제어합니다.");
     options.add_options()
         ("c,com", "Com Port", cxxopts::value<std::string>())
-        ("b,baudrate", "Baud Rate", cxxopts::value<int>()->default_value(115200))
+        ("b,baudrate", "Baud Rate", cxxopts::value<int>()->default_value("115200"))
         ;
-    auto result = options.parse(argc, argv);
-
     serial s;
-    spdlog::info("open: {}", s.open(result["baudrate"].as<int>(), result["com"].as<std::string>()));
+    try { 
+        auto result = options.parse(argc, argv);
+        spdlog::info("open: {}", s.open(result["baudrate"].as<int>(), result["com"].as<std::string>()));
+    }catch (std::exception e) { 
+        spdlog::error("{}", e.what());
+        spdlog::error("{}", options.help());
+        return 0;
+    }    
+
             //                 STX   TYPE  ID  DATA01 02     03   04   CHK   ETX
 
     app().setLogPath("./")
